06d3b9d9ed1a50a90b98d836d2725315f3f1abb3
CaueFer <cauefernandescaetano@gmail.com>
2024-07-17 16:57:03 -0300

    add responsive home

--- src/app/layouts/nav/nav.component.ts
+++ src/app/layouts/nav/nav.component.ts
@@ -1,13 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
+  sidebarOpen: boolean = false;
+
+  constructor(){};
 
 }
